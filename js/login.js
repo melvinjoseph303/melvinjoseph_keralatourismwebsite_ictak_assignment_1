4265fa00b8338b860c027bff236ac78054d66a18
melvin <melvinjoseph303@gmail.com>
2021-04-13 18:54:15 +0530

    login js file updated

--- js/login.js
+++ js/login.js
@@ -1,14 +1,10 @@
-//function validate(){
 
-    //var emailField = document.getElementById("inputEmail4").value;
-    //var inputtxt = document.getElementById("phoneno").value;
-    var emailerr = document.getElementById("emailerror");
-    var phonenoerr = document.getElementById("phonenoerror");
-    //var result = validateEmail(emailField)
     var result1;
     var result2;
+    var result3;
 
     function validateEmail(emailField){
+      var emailerr = document.getElementById("emailerror");
         //var reg = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/;
         var reg = /^([A-Za-z0-9\.-]+)@([A-Za-z0-9\-]+).([a-z]{2,3})(.[a-z]{2,3})?$/;
         if (reg.test(emailField.value) == false) 
@@ -30,6 +26,7 @@
     function phonenumber(inputtxt)
     {
       
+      var phonenoerr = document.getElementById("phonenoerror");
       var phoneno = /^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/;
       if(inputtxt.value.match(phoneno))
          {
@@ -47,11 +44,54 @@
            return false;
          }
     }
+
+    function validatePassword(pwd){
+      var pwderr = document.getElementById("pwderror");
+      if(pwd == ""){
+        pwderr.innerHTML = "Invalid";
+        pwderr.style.color = "Red";
+        result3 = false;
+        console.log("result3:"+result3);
+        return false;
+      }
+      else{
+        pwderr.innerHTML = "Valid";
+        pwderr.style.color = "Green";
+        result3 = true;
+        console.log("result3:"+result3);
+        return true;
+      }
+    }
 //}
 
-function validate(){ 
-  var fresult = result1 && result2;
-  return fresult;
+function validateSignup(){ 
+  var mailField = document.getElementById("inputEmail4").value;
+  var phonefield = document.getElementById("phoneno").value;
+  var pwdField = document.getElementById("psw-input").value;
+  //var phonenoerr = document.getElementById("phonenoerror");  
+  //var fresult1 = result1 && result2 && result3;
+  var fresult1 = result1 && result2;
+  if(mailField == ""||phonefield == ""||pwdField == ""){
+    return false;
+  }
+  else{
+    console.log("fresult1:"+fresult1);
+  return fresult1;
+  }
+  
+}
+
+function validateLogin(){ 
+  var mailField = document.getElementById("inputEmail4").value;
+  var pwdField = document.getElementById("psw-input").value;
+  // var fresult2 = result1 && result3;
+  // return fresult2;
+  if(mailField == ""||pwdField == ""){
+    return false;
+  }
+  else{
+    return true;
+  }
 }
 
 
